0783489b10b566c2acf8dc8c59dd9be81da04273
tientrung26101991 <tientrung26101991@gmail.comm>
2021-07-10 00:35:56 +0900

    Update

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -38,6 +38,8 @@
                     alert("password is required")
                 }
                 alert("error!!!!!")
+
+                     bdjfhdjkfhsjkdfhjk
             }
         }   
     }
