8c2f57d500f1a6b8a74a883d287ac57a946859af
tientrung26101991 <tientrung26101991@gmail.comm>
2021-07-10 15:05:43 +0900

    Update v2 Bootstrap

--- src/components/BootStrap.vue
+++ src/components/BootStrap.vue
@@ -1,13 +1,6 @@
 <template>
-    <div>
-        <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-            <b-dropdown-item>First Action</b-dropdown-item>
-            <b-dropdown-item>Second Action</b-dropdown-item>
-            <b-dropdown-item>Third Action</b-dropdown-item>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item active>Active action</b-dropdown-item>
-            <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-        </b-dropdown>
+    <div class="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
     </div>
 </template>
 
