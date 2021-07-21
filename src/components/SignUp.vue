284c4a05f985ae7f269a5b8fd67503b09f1f670d
tientrung26101991 <tientrung26101991@gmail.com>
2021-07-21 16:11:24 +0900

    Update Signup

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -9,20 +9,36 @@
 
           <div class="form-group">
             <label>Full Name</label>
-            <input type="text" class="form-control form-control-lg" />
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              v-model="fullname"
+            />
           </div>
 
           <div class="form-group">
             <label>Email address</label>
-            <input type="email" class="form-control form-control-lg" />
+            <input
+              type="email"
+              class="form-control form-control-lg"
+              v-model="email"
+            />
           </div>
 
           <div class="form-group">
             <label>Password</label>
-            <input type="password" class="form-control form-control-lg" />
+            <input
+              type="password"
+              class="form-control form-control-lg"
+              v-model="pass"
+            />
           </div>
 
-          <button type="submit" class="btn btn-dark btn-lg btn-block">
+          <button
+            type="submit"
+            class="btn btn-dark btn-lg btn-block"
+            v-on:click="signup"
+          >
             Sign Up
           </button>
 
@@ -38,11 +54,24 @@
 
 <script>
 import Navigation from "./Navigation.vue";
+
 export default {
   components: { Navigation },
+
   name: "SignUp",
+
   data() {
-    return {};
+    return {
+      fullname: "",
+      email: "",
+      pass: "",
+    };
+  },
+
+  methods: {
+    signup() {
+      console.warn(this.fullname + this.email + this.pass);
+    },
   },
 };
 </script>
@@ -60,5 +89,16 @@ export default {
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+}
+
+.vue-tempalte label {
+  margin-right: 690px;
+  margin-top: 20px;
+}
+
+.vue-tempalte button {
+  margin-top: 30px;
+  width: 260px;
+  background: skyblue;
 }
 </style>
